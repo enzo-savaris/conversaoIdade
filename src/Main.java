f12f41d320fb425d008140c3386a716b8a705d89
enzo-savaris <savarisenzo2@gmail.com>
2025-08-07 21:56:53 -0300

    Atividade de conversão de idade finalizada.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.text.DecimalFormat;
+
 public class Main {
-    public static void main (string[] args){
-        System.out.println("Hello World");
+    public static void main (String[] args){
+        DecimalFormat df = new DecimalFormat("#,###.00");
+        int idade    = 18;
+        int dias     = idade * 365;
+        int horas    = dias * 24;
+        int minutos  = horas * 60;
+        int segundos = minutos * 60;
+
+        System.out.println("Você já viveu aproximadamente: ");
+        System.out.println(df.format(dias) + " Dias");
+        System.out.println(df.format(horas) + " Horas");
+        System.out.println(df.format(minutos) + " Minutos");
+        System.out.println(df.format(segundos) + " Segundos");
     }
 }
